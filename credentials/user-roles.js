98a9aec0cbf1f5d5d4c525c0402d0db10b5ad6df
Arlingga Wirawan Jayasaputra <arlingga.wirawan@gpi.id>
2024-08-12 18:02:48 +0700

    masking

--- credentials/user-roles.js
+++ credentials/user-roles.js
@@ -1,6 +1,6 @@
 // Define an array of objects containing usernames, passwords, and access roles
 const userCredentials = [
-    { username: 'arlingga.wirawan@gpi.id', password: 'arlingga-123', role: 'super-admin' }
+    { username: 'xxxxxawan@gpi.id', password: 'xxxxxxxxx', role: 'super-admin' }
     // add more account and roles for create scenario
 ];
 
